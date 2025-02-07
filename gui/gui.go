85f2608bd23b96aea8f6ee9e2183fcdfb52241e4
joshfah <josh.viktor.fahn@gmail.com>
2025-02-07 02:11:20 +0100

    added basic gui

--- gui/gui.go
+++ gui/gui.go
@@ -1,9 +1,18 @@
 package gui
 
-import "cogentcore.org/core/core"
+import (
+	"fmt"
+
+	"cogentcore.org/core/core"
+	"cogentcore.org/core/events"
+)
 
 func Gui() {
 	body := core.NewBody()
-	core.NewText(body)
+	core.NewText(body).SetText("Enter URL for Anime here: ")
+	URLText := core.NewTextField(body).SetPlaceholder("https://aniworld.to/...")
+	core.NewButton(body).SetText("Search").OnClick(func(e events.Event) {
+		fmt.Println(URLText.Text())
+	})
 	body.RunMainWindow()
 }
